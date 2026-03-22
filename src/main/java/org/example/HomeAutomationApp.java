55f480c05dad87327a34990916dcb011290c9a62
y0k0s <kurpatowph@yandex.ru>
2026-03-22 19:10:41 +0300

    .

--- src/main/java/org/example/HomeAutomationApp.java
+++ src/main/java/org/example/HomeAutomationApp.java
@@ -1,5 +1,10 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 interface Controllable {
     void turnOn();
     void  turnOff();
@@ -49,5 +54,41 @@ class Light extends Device implements Controllable, Alertable {
     public void turnOff() {
         isOn = false;
         System.out.println(name + " light turned off.");
+    }
+
+    class Thermostat extends Device implements Controllable, Measurable {
+        private boolean isHeating;
+        private  double currentTemperature;
+
+        public Thermostat(String name) {
+            super(name, "Thermostat");
+            currentTemperature = 20.0;
+        }
+
+        @Override
+        public void turnOn() {
+         isHeating = true;
+            System.out.println(name + " thermostat turned on.");
+        }
+
+        @Override
+        public void turnOff() {
+            isHeating = false;
+            System.out.println(name + " thermostat turned off.");
+        }
+
+        @Override
+        public double getMeasurement() {
+            return currentTemperature;
+        }
+    }
+    class HomeAutomationSystem {
+        private List<Device> devices;
+        private Map<String, Device> deviceMap;
+
+        public HomeAutomationSystem() {
+            devices = new ArrayList<>();
+            deviceMap = new HashMap<>();
+        }
     }
 }
